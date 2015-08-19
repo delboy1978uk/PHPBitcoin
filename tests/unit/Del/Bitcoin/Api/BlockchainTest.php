a22c7e898d4b4dac0a1f2455f11edf46eea41110
delboy1978uk <delboy1978uk@gmail.com>
2015-08-19 02:53:50 +0100

    tests for all blockchain api calls with no args

--- tests/unit/Del/Bitcoin/Api/BlockchainTest.php
+++ tests/unit/Del/Bitcoin/Api/BlockchainTest.php
@@ -53,5 +53,68 @@ class BlockchainTest extends \Codeception\TestCase\Test
 
 
 
+    public function testGetBlockchainInfo()
+    {
+        $info = json_decode($this->api->getBlockChainInfo(),true);
+        $this->assertArrayHasKey('result',$info);
+        $this->assertEquals('test',$info['result']['chain']);
+    }
+
+
+
+
+    public function testGetBlockCount()
+    {
+        $info = json_decode($this->api->getBlockCount(),true);
+        $this->assertArrayHasKey('result',$info);
+    }
+
+
+
+
+    public function testGetChainTips()
+    {
+        $info = json_decode($this->api->getChainTips(),true);
+        $this->assertArrayHasKey('result',$info);
+        $this->assertArrayHasKey('height',$info['result'][0]);
+        $this->assertTrue(is_int($info['result'][0]['height']));
+    }
+
+
+
+
+    public function testGetDifficulty()
+    {
+        $info = json_decode($this->api->getChainTips(),true);
+        $this->assertArrayHasKey('result',$info);
+        $this->assertArrayHasKey('height',$info['result'][0]);
+        $this->assertTrue(is_numeric($info['result'][0]['height']));
+    }
+
+
+
+
+    public function testGetMemPoolInfo()
+    {
+        $info = json_decode($this->api->getMemPoolInfo(),true);
+        $this->assertArrayHasKey('result',$info);
+        $this->assertArrayHasKey('size',$info['result']);
+        $this->assertTrue(is_numeric($info['result']['size']));
+    }
+
+
+
+
+    public function testGetTxOutSetInfo()
+    {
+        $info = json_decode($this->api->getTxOutSetInfo(),true);
+        $this->assertArrayHasKey('result',$info);
+        $this->assertArrayHasKey('txouts',$info['result']);
+        $this->assertTrue(is_numeric($info['result']['txouts']));
+    }
+
+
+
+
 
 }
