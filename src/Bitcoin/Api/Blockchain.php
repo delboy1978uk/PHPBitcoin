2553f205a6119d5c1d25609fe24aeb7ffe89a9a3
delboy1978uk <delboy1978uk@gmail.com>
2015-08-19 01:06:43 +0100

    Added API calls for Blockchain API

--- src/Bitcoin/Api/Blockchain.php
+++ src/Bitcoin/Api/Blockchain.php
@@ -11,63 +11,145 @@ namespace Del\Bitcoin\Api;
 class Blockchain extends AbstractApi
 {
 
+    /**
+     * returns the header hash of the most recent block on the best block chain.
+     *
+     * @return mixed
+     */
     public function getBestBlockHash()
     {
-
+        return $this->send('getbestblockhash');
     }
 
-    public function getBlock()
+    /**
+     * The getblock RPC gets a block with a particular header hash from
+     * the local block database either as a JSON object
+     * or as a serialized block.
+     *
+     * @param string $header_hash
+     * @param bool $json
+     * @return mixed
+     */
+    public function getBlock($header_hash,$json = true)
     {
-
+        return $this->send('getblock',[$header_hash,$json]);
     }
 
+    /**
+     * The getblockchaininfo RPC provides information about
+     * the current state of the block chain.
+     *
+     * @return mixed
+     */
     public function getBlockChainInfo()
     {
-
+        return $this->send('getblockchaininfo');
     }
 
+    /**
+     * The getblockcount RPC returns the number of blocks in
+     * the local best block chain.
+     *
+     * @return mixed
+     */
     public function getBlockCount()
     {
-
+        return $this->send('getblockcount');
     }
 
-    public function getBlockHash()
+    /**
+     * The getblockhash RPC returns the header hash of a block
+     * at the given height in the local best block chain.
+     *
+     * @param $height
+     * @return mixed
+     */
+    public function getBlockHash($height)
     {
-
+        return $this->send('getblockhash',[$height]);
     }
 
+    /**
+     * The getchaintips RPC returns information about the
+     * highest-height block (tip) of each local block chain.
+     *
+     * @return mixed
+     */
     public function getChainTips()
     {
-
+        return $this->send('getchaintips');
     }
 
+    /**
+     * The getdifficulty RPC
+     *
+     * @return mixed
+     */
     public function getDifficulty()
     {
-
+        return $this->send('getdifficulty');
     }
 
+    /**
+     * The getmempoolinfo RPC returns information about
+     * the node’s current transaction memory pool.
+     *
+     * @return mixed
+     */
     public function getMemPoolInfo()
     {
-
+        return $this->send('getmempoolinfo');
     }
 
-    public function getRawMemPool()
+    /**
+     * The getrawmempool RPC returns all transaction identifiers (TXIDs)
+     * in the memory pool as a JSON array, or detailed information about
+     * each transaction in the memory pool as a JSON object.
+     *
+     * @param bool $verbose
+     * @return mixed
+     */
+    public function getRawMemPool($verbose = false)
     {
-
+        return $this->send('getrawmempool',[$verbose]);
     }
 
-    public function getTxOut()
+    /**
+     * The gettxout RPC returns details about a transaction output.
+     * Only unspent transaction outputs (UTXOs) are guaranteed to be
+     * available.
+     *
+     * @param string $txid a hex id
+     * @return mixed
+     */
+    public function getTxOut($txid)
     {
-
+        return $this->send('gettxout',[$txid]);
     }
 
+    /**
+     * The gettxoutsetinfo RPC returns statistics about the confirmed
+     * unspent transaction output (UTXO) set. Note that this call may
+     * take some time and that it only counts outputs from confirmed
+     * transactions—it does not count outputs from the memory pool.
+     *
+     * @return mixed
+     */
     public function getTxOutSetInfo()
     {
-
+        return $this->send('gettxoutsetinfo');
     }
 
-    public function verifyChain()
+    /**
+     * The verifychain RPC verifies each entry in the local
+     * block chain database.
+     *
+     * @param $check_level
+     * @param $num_blocks
+     * @return mixed
+     */
+    public function verifyChain($check_level, $num_blocks)
     {
-
+        return $this->send('verifychain',[$check_level,$num_blocks]);
     }
 }
--- src/Bitcoin/Api/Control.php
+++ src/Bitcoin/Api/Control.php
@@ -10,17 +10,31 @@ namespace Del\Bitcoin\Api;
 
 class Control extends AbstractApi
 {
-
+    /**
+     * The getinfo RPC prints various information about the node and the network.
+     * @return mixed
+     */
     public function getInfo()
     {
         return $this->send('getinfo');
     }
 
+    /**
+     * The help RPC lists all available public RPC commands,
+     * or gets help for the specified RPC. Commands which are
+     * unavailable will not be listed, such as wallet RPCs if
+     * wallet support is disabled.
+     * @return mixed
+     */
     public function help()
     {
         return $this->send('help');
     }
 
+    /**
+     * The stop RPC safely shuts down the Bitcoin Core server.
+     * @return mixed
+     */
     public function stop()
     {
         return $this->send('stop');
